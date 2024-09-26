4465b0649781c35a8ed39e94af6ac5958eba8b5c
Maya Kazakova <anais.maya@gmail.com>
2024-09-26 22:55:11 +0100

    add some logic

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    const newContact = createFakeContact();
+
+    const updatedContacts = [...existingContacts, newContact];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error(`Error adding contact: ${error.message}`);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,21 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+    console.log(`Successfully generated and added ${number} contacts.`);
+  } catch (error) {
+    console.error(`Error generating contacts: ${error.message}`);
+  }
+};
 
 generateContacts(5);
+generateContacts(10);
